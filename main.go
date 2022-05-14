59a16bab920b0e403eeb36ef55f486e129e3545c
Graham Albachten <albachteng@gmail.com>
2022-05-14 11:55:11 -0500

    add vector scaling and summation, length

--- main.go
+++ main.go
@@ -10,25 +10,46 @@ type vector struct {
 	x, y float64
 }
 
+/* takes two floats and returns a reference to a new vector */
 func newVector(x, y float64) *vector {
 	v := vector{x, y}
 	return &v
 }
 
+/* returns the numerical value of a vector's length property */
 func (v *vector) length() float64 {
 	return math.Sqrt(v.x*v.x + v.y*v.y)
 }
 
-func getLength(v vector) float64 {
-	return v.length()
+/* sums two vectors with vector addition and returns a pointer to the new
+vector that results  */
+func (v *vector) sumVector(n vector) *vector {
+	x := v.x + n.x
+	y := v.y + n.y
+	return newVector(x, y)
 }
 
+/* the same as above, but allows you to describe the vector on the fly */
+func (v *vector) add(x, y float64) *vector {
+	n := *newVector(x, y)
+	return v.sumVector(n)
+}
+
+/* multiplies the x and y values of a vector by a scalar (float)
+and returns a pointer to the new vector */
+func (v *vector) scale(s float64) *vector {
+	n := newVector(v.x*s, v.y*s)
+	return n
+}
+
+
+
 func main() {
-	fmt.Println("hello world")
 	unitVector := *newVector(1.0, 1.0)
-	fmt.Println(math.Round(1.5))
-	fmt.Println(unitVector)
-	fmt.Println(reflect.TypeOf(unitVector))
-	fmt.Println(getLength(unitVector)) // expect root 2
-	fmt.Println(unitVector.length())   // expect root 2
+	fmt.Println(reflect.TypeOf(unitVector)) // expect vector
+	fmt.Println(unitVector.length())        // expect root 2
+	anotherVector := unitVector.add(1, 0)   // add leaves unitVector untouched
+	fmt.Println(anotherVector.length())     // expect NOT root 2
+	fmt.Println(*unitVector.sumVector(*newVector(2, 3)))
+	fmt.Println(anotherVector.scale(-2.1).length())
 }
