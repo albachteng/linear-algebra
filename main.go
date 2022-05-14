3e3e0dc6104f3c007f7e726b3eb02ef2f23f1480
Graham Albachten <albachteng@gmail.com>
2022-05-14 13:29:58 -0500

    make sumVector and vector type private

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 	fmt.Println(unitVector.Length())        // expect root 2
 	anotherVector := unitVector.Add(1, 0)   // add leaves unitVector untouched
 	fmt.Println(anotherVector.Length())     // expect NOT root 2
-	fmt.Println(*unitVector.SumVector(*vector.NewVector(2, 3)))
+	fmt.Println(*unitVector.Add(2, 3))
 	fmt.Println(anotherVector.Scale(-2.1).Length())
 }
--- vector/vector.go
+++ vector/vector.go
@@ -2,38 +2,38 @@ package vector
 
 import "math"
 
-type Vector struct {
+type vector struct {
 	x, y float64
 }
 
 /* takes two floats and returns a reference to a new vector */
-func NewVector(x, y float64) *Vector {
-	v := Vector{x, y}
+func NewVector(x, y float64) *vector {
+	v := vector{x, y}
 	return &v
 }
 
 /* returns the numerical value of a vector's length property */
-func (v *Vector) Length() float64 {
+func (v *vector) Length() float64 {
 	return math.Sqrt(v.x*v.x + v.y*v.y)
 }
 
 /* sums two vectors with vector addition and returns a pointer to the new
 vector that results  */
-func (v *Vector) SumVector(n Vector) *Vector {
+func (v *vector) sumVector(n vector) *vector {
 	x := v.x + n.x
 	y := v.y + n.y
 	return NewVector(x, y)
 }
 
 /* the same as above, but allows you to describe the vector on the fly */
-func (v *Vector) Add(x, y float64) *Vector {
+func (v *vector) Add(x, y float64) *vector {
 	n := *NewVector(x, y)
-	return v.SumVector(n)
+	return v.sumVector(n)
 }
 
 /* multiplies the x and y values of a vector by a scalar (float)
 and returns a pointer to the new vector */
-func (v *Vector) Scale(s float64) *Vector {
+func (v *vector) Scale(s float64) *vector {
 	n := NewVector(v.x*s, v.y*s)
 	return n
 }
