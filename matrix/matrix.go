442abba30c7ff5fcf37725f090cdd14968a91440
Graham Albachten <albachteng@gmail.com>
2022-05-14 18:34:39 -0500

    clean notes

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -4,6 +4,8 @@ import (
 	"github.com/albachteng/linear-algebra/vector"
 )
 
+/* a 2-dimensional matrix that describes 
+a linear transformation */
 type matrix struct {
 	iHat vector.Vector
 	jHat vector.Vector
@@ -13,8 +15,8 @@ func NewMatrix(v1, v2 vector.Vector) *matrix {
 	return &matrix{v1, v2}
 }
 
-/* on a given matrix and given a transformation vector,
-returns the new matrix */
+/* on a given matrix and given a vector to transform,
+returns the transformed vector */
 func (m *matrix) LinearTransform(v vector.Vector) *vector.Vector {
 	x := m.iHat.Scale(v.X)
 	y := m.jHat.Scale(v.Y)
