d62a33f02f3ffe64c6b8b9d11294b988b005c1a7
Graham Albachten <albachteng@gmail.com>
2022-05-14 20:36:28 -0500

    add matrix_test.go

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -4,7 +4,7 @@ import (
 	"github.com/albachteng/linear-algebra/vector"
 )
 
-/* a 2-dimensional matrix that describes 
+/* a 2-dimensional matrix that describes
 a linear transformation */
 type matrix struct {
 	iHat vector.Vector
@@ -20,5 +20,13 @@ returns the transformed vector */
 func (m *matrix) LinearTransform(v vector.Vector) *vector.Vector {
 	x := m.iHat.Scale(v.X)
 	y := m.jHat.Scale(v.Y)
-	return x.SumVector(*y) 
+	return x.SumVector(*y)
+}
+
+/* multiplies the matrix by another matrix to return a new one
+applies a rotation and then a shear, where s is the shear */
+func (m *matrix) Composition(s *matrix) *matrix {
+	i := m.LinearTransform(s.iHat)
+	j := m.LinearTransform(s.jHat)
+	return NewMatrix(*i, *j)
 }
