5447930b07f229ca2b6db89096b643098328ae53
Graham Albachten <albachteng@gmail.com>
2022-05-15 19:15:27 -0500

    add test for vector

--- main.go
+++ main.go
@@ -3,20 +3,12 @@ package main
 import (
 	"fmt"
 	"reflect"
-
 	"github.com/albachteng/linear-algebra/matrix"
 	"github.com/albachteng/linear-algebra/vector"
 )
 
 func main() {
-	unitVector := *vector.NewVector(1, 1)
-	fmt.Println(reflect.TypeOf(unitVector)) // expect vector
-	fmt.Println(unitVector.Length())        // expect root 2
-	// anotherVector := unitVector.Add(1, 0)   // add leaves unitVector untouched
 	anotherVector := *vector.NewVector(-1, 2)
-	// fmt.Println(anotherVector.Length())     // expect NOT root 2
-	// fmt.Println(*unitVector.Add(2, 3))
-	// fmt.Println(anotherVector.Scale(-2.1).Length())
 	i := *vector.NewVector(1, -2)
 	j := *vector.NewVector(3, 0)
 	matrix := *matrix.NewMatrix(i, j)
---
+++ vector/vector_test.go
@@ -0,0 +1,14 @@
+package vector
+
+import (
+	"testing"
+	"math"
+)
+
+func TestVector(t *testing.T) {
+	unitVector := *NewVector(1, 1)
+	rootTwo := unitVector.Length()        // expect root 2
+	if rootTwo != math.Sqrt(2) {
+		t.Errorf("expected length of unit vector to be root 2, got %f", rootTwo)
+	}
+}
