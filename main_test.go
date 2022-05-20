f69a68b9983b24c5c0ffd2a29f07c947a52da2b6
Graham Albachten <albachteng@gmail.com>
2022-05-19 20:17:20 -0500

    add main_test.go and moved our test case over

---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/albachteng/linear-algebra/matrix"
+	"github.com/albachteng/linear-algebra/vector"
+)
+
+func TestVector(t *testing.T) {
+	someVector := *vector.NewVector(-1, 2)
+	i := *vector.NewVector(1, -2)
+	j := *vector.NewVector(3, 0)
+	matrix := *matrix.NewMatrix(i, j)
+	got := *matrix.LinearTransform(someVector) // expect 5,2
+	if got.X != 5 || got.Y != 2 {
+		t.Errorf("expected transform {5, 2}, got {%f, %f}", got.X, got.Y)
+	}
+}
