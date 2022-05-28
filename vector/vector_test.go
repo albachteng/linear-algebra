86865618b6e54daa889e62a26ce640ace3a549e4
Graham Albachten <albachteng@gmail.com>
2022-05-28 18:02:06 -0500

    refactor tests to use indices instead of x and y

--- vector/vector_test.go
+++ vector/vector_test.go
@@ -12,15 +12,15 @@ func TestVector(t *testing.T) {
 		t.Errorf("expected length of unit vector to be root 2, got %f", rootTwo)
 	}
 	scaledUnitVector := unitVector.Scale(2)
-	if scaledUnitVector.X != 2 {
-		t.Errorf("expected scaled X to be 1, got %f", scaledUnitVector.X)
+	if scaledUnitVector[0] != 2 {
+		t.Errorf("expected scaled X to be 1, got %f", scaledUnitVector[0])
 	}
-	span := GetSpan(scaledUnitVector, &unitVector)
-	if span != 1 {
-		t.Errorf("expected a span of 1, got %d", span)
-	}
-	sum := unitVector.SumVector(*scaledUnitVector)
-	if sum.X != 3 || sum.Y != 3 {
-		t.Errorf("expected a sum of 1 and 1, got %f and %f", sum.X, sum.Y)
+	// span := GetSpan(scaledUnitVector, &unitVector)
+	// if span != 1 {
+	// 	t.Errorf("expected a span of 1, got %d", span)
+	// }
+	sum := unitVector.SumVector(scaledUnitVector)
+	if sum[0] != 3 || sum[1] != 3 {
+		t.Errorf("expected a sum of 1 and 1, got %f and %f", sum[0], sum[1])
 	}
 }
