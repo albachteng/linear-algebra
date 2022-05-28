60e51372b9bccff5f804289a681087d85b6b3983
Graham Albachten <albachteng@gmail.com>
2022-05-28 17:58:24 -0500

    refactor for vectors of higher dimensions

--- vector/vector.go
+++ vector/vector.go
@@ -1,45 +1,71 @@
 package vector
 
-import "math"
+import (
+	"math"
+	"errors"
+)
 
-type Vector struct {
-	X, Y float64
-}
+type Vector []float64
 
 /* returns the numerical value of a vector's length property */
-func (v *Vector) Length() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+func (v Vector) Length() float64 {
+	sum := 0.0
+	for _, n := range v {
+		sum += n * n
+	}
+	return math.Sqrt(sum)
 }
 
 /* sums two vectors with vector addition and returns a pointer to the new
 vector that results  */
-func (v *Vector) SumVector(n Vector) *Vector {
-	x := v.X + n.X
-	y := v.Y + n.Y
-	return &Vector{x, y}
+func (v Vector) SumVector(w Vector) Vector {
+	var sums = make(Vector, 2)
+	for i, num := range v {
+		sums[i] =  num + w[i]
+	}
+	return sums 
 }
 
-/* multiplies the x and y values of a vector by a scalar (float)
+/* multiplies the values of a vector by a scalar (float)
 and returns a pointer to the new vector */
-func (v *Vector) Scale(s float64) *Vector {
-	n := Vector{v.X * s, v.Y * s}
-	return &n
+func (v Vector) Scale(s float64) Vector {
+	var scaled = make(Vector, 2)
+	for i, num := range v {
+		scaled[i] = num * s	
+	} 
+	return scaled
 }
 
 /* returns the slope of a given vector */
-func (v *Vector) Slope() float64 {
-	return v.Y / v.X
-}
+// func (v Vector) Slope() float64 {
+// 	return v.Y / v.X
+// }
 
-/* most vectors have a span of all of 2 dimensional space, 
-but when their slope is the same, their span is 1 dimension 
-- a single line. If both vectors are zero, their span is only the origin. */ 
-func GetSpan(a *Vector, b *Vector) uint8 {
-	if a.Slope() == 0 && b.Slope() == 0 {
-		return 0
+/* most vectors have a span of all of 2 dimensional space,
+but when their slope is the same, their span is 1 dimension
+- a single line. If both vectors are zero, their span is only the origin. */
+// func GetSpan(a *Vector, b *Vector) uint8 {
+// 	if a.Slope() == 0 && b.Slope() == 0 {
+// 		return 0
+// 	}
+// 	if a.Slope() == b.Slope() {
+// 		return 1
+// 	}
+// 	return 2
+// }
+
+/* sums the products of each value in two vectors of the same size */ 
+func DotProduct(v Vector, w Vector) (float64, error) {
+	if v.Length() != w.Length() {
+		return 0, errors.New("DotProduct called with vectors of unequal length")
 	}
-	if a.Slope() == b.Slope() {
-		return 1
+	var products = make([]float64, 2)
+	for i, num := range v {
+		products[i] = num * w[i]
 	}
-	return 2
+	var sum float64
+	for _, num := range products {
+		sum += num
+	}
+	return sum, nil
 }
